0e72b659bf755b136a138acd8f8474f92083828f
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2024-03-12 13:45:57 +0545

    Initial Commit

---
+++ E-Commerce/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  E-Commerce
+//
+//  Created by Prabesh Shrestha on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
